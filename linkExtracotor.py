4d06b4256abaa9bf657647f64df6990b4755357b
yiqiwang(王一棋) <yiqiwang@tencent.com>
2019-03-07 15:30:23 +0800

    Delete unused files

--- linkExtracotor.py
+++
@@ -1,8 +0,0 @@
-from scrapy.http import HtmlResponse
-from scrapy.linkextractors import LinkExtractor
-
-html1 = open('example1.html').read()
-response1 = HtmlResponse(url='http://example1.com', body=html1, encoding='utf8')
-le = LinkExtractor()
-links = le.extract_links(response1)
-print([link.url for link in links])
